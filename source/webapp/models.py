f98437679c32ee362690ffea9bc54aa91c64a344
voodoo <voodoo.people.j@gmail.com>
2020-08-01 13:12:57 +0600

    Добавлена модель гостевой книги

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    DEFAULT_STATUS = 'active'
+    STATUS_CHOICES = [
+        (DEFAULT_STATUS, 'активно'),
+        ('blocked', 'заблокировано')
+    ]
+
+    name = models.CharField(max_length=100, verbose_name='Имя')
+    email = models.EmailField(max_length=100, verbose_name='Почта')
+    descriptions = models.TextField(max_length=2000, verbose_name='Описание')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Время изменения')
+    status = models.CharField(max_length=20, default=DEFAULT_STATUS, choices=STATUS_CHOICES, verbose_name='Статус')
+
+    def __str__(self):
+        return f'{self.name} - {self.status}'
+
